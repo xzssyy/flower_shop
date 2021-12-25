ec1f762eb56d281c3294ecf6f171dbd13a59a30f
panyue <xzssyy@gmail.com>
2021-12-25 22:34:47 +0800

    1

---
+++ Functions.py
@@ -0,0 +1,35 @@
+def initialize(ui, db):
+    pass
+
+from PyQt5.QtSql import *
+
+#判断某个table中的某col是否存在target值
+def judge(table, col, target):
+        #target = str(target)
+        target = "'"+target+"'"
+        query = QSqlQuery()
+        sql = "SELECT EXISTS(SELECT * FROM {} WHERE {} = {}) as p".format(str(table), str(col), target)
+
+        if query.exec(sql):
+            t = query.record().indexOf('p')
+            query.next()
+            judge = query.value(t)
+
+            if judge:
+                return 1
+            else:
+                return 0
+        else:
+            return -1
+
+
+
+def buy(ui):
+    name = ui.input_name.text()
+    nums = ui.input_nums.text()
+
+    query = QSqlQuery()
+    sql
+
+    #price = None
+    #ui.output_buylist.setText(str(name))
